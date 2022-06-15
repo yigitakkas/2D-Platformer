5fb81b80afc914c4121829680d80fbc9d9574c13
Yiğit Akkaş <yigitakkasbjk@hotmail.com>
2022-06-16 00:51:21 +0300

    added Jumping for player

--- 2D-Platformer/Assets/Scripts/PlayerController.cs
+++ 2D-Platformer/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 10f;
-    public float gravity = 20f;
+    public float walkSpeed = 15f;
+    public float gravity = 35f;
+    public float jumpSpeed = 15f;
+
+    public bool isJumping;
+    private bool _startJump;
+    private bool _releaseJump;
 
     private Vector2 _input;
     private Vector2 _moveDirection;
@@ -23,15 +28,27 @@ public class PlayerController : MonoBehaviour
         _moveDirection.x = _input.x; //x value of input from the player
         _moveDirection.x *= walkSpeed; //affect the x value with walk speed
 
-        if(_characterController2D.below)
+        if(_characterController2D.below) //if player is on the round
         {
-
+            if(_startJump)
+            {
+                _startJump = false;
+                _moveDirection.y = jumpSpeed;
+                isJumping = true;
+            }
         } 
-        else
+        else //if player is in the air
         {
+            if(_releaseJump)
+            {
+                _releaseJump = false;
+                if(_moveDirection.y>0)
+                {
+                    _moveDirection.y *= .5f;
+                }
+            }
             _moveDirection.y -= gravity * Time.deltaTime; //add gravity to y value
         }
-
 
         _characterController2D.Move(_moveDirection * Time.deltaTime);
     }
@@ -40,4 +57,17 @@ public class PlayerController : MonoBehaviour
     {
         _input = context.ReadValue<Vector2>();
     }
+
+    public void OnJump (InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            _startJump = true;
+        }
+        else if (context.canceled)
+        {
+            _releaseJump = true;
+        }
+    }
+
 }
