7efbd96c3b9decda2ac4dbe0057391775b54b049
Yiğit Akkaş <yigitakkasbjk@hotmail.com>
2022-06-15 17:51:35 +0300

    added Character and Player Controller

--- 2D-Platformer/Assets/CharacterController2D.cs
+++ 2D-Platformer/Assets/CharacterController2D.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
+
 {
-    // Start is called before the first frame update
+    private Vector2 _moveAmount;
+    private Vector2 _currPosition;
+    private Vector2 _lastPosition;
+    private Rigidbody2D _rigidbody2D;
+    private CapsuleCollider2D _capsuleCollider2D;
+
     void Start()
+    {
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        _capsuleCollider2D = gameObject.GetComponent<CapsuleCollider2D>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate() //to put physics simulations in fixed update is a better practice than putting them in update method
     {
-        
+        _lastPosition = _rigidbody2D.position;
+        _currPosition = _lastPosition + _moveAmount;
+        _rigidbody2D.MovePosition(_currPosition);
+        _moveAmount = Vector2.zero; //move amount did its job so reset the value to zero
+    }
+
+    public void Move(Vector2 movement)
+    {
+        _moveAmount += movement;
     }
 }
--- 2D-Platformer/Assets/PlayerController.cs
+++ 2D-Platformer/Assets/PlayerController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float walkSpeed = 10f;
+    public float gravity = 20f;
+
+    private Vector2 _input;
+    private Vector2 _moveDirection;
+    private CharacterController2D _characterController2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _characterController2D = gameObject.GetComponent<CharacterController2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _moveDirection.x = _input.x; //x value of input from the player
+        _moveDirection.x *= walkSpeed; //affect the x value with walk speed
+
+        _moveDirection.y -= gravity * Time.deltaTime; //add gravity to y value
+
+        _characterController2D.Move(_moveDirection * Time.deltaTime);
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
     }
 }
