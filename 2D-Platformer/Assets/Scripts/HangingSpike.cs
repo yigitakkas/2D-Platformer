22889ee146647d44d5e921be3697b821388c4d25
Yiğit Akkaş <yigitakkasbjk@hotmail.com>
2022-07-15 18:22:02 +0300

    created second level and instantiated third
    designed second level while making player able to use a new skill(double jump)

--- 2D-Platformer/Assets/Scripts/HangingSpike.cs
+++ 2D-Platformer/Assets/Scripts/HangingSpike.cs
@@ -8,10 +8,13 @@ public class HangingSpike : MonoBehaviour
     [SerializeField] LayerMask playerLayer;
     private RaycastHit2D playerHit;
     private bool playerDetected;
+    private SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
 
     private void Awake()
     {
         spikeBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -27,8 +30,9 @@ public class HangingSpike : MonoBehaviour
         Debug.DrawRay(transform.position, Vector2.down * 10f, Color.red);
         if(playerHit)
         {
+            spriteRenderer.sprite = newSprite;
             playerDetected = true;
-            spikeBody.gravityScale = 4f;
+            spikeBody.gravityScale = 3f;
             Invoke("DeactivateObject", 3f);
         }
     }
